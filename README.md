e256d356d5cd4ad8c6da663873b6be6efd36ca63
Woofer21 <potatoh99@gmail.com>
2022-08-05 17:40:58 -0400

    webhook/custom channel & global/local emotes

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # Emoji List Maker
 
-Grabs emotes from all servers its in and sends a webhook to the URL specified
+Grabs emotes from the guild the command was ran in and sends it to a channel or predefined webhook.
 
 ## Requirements
 - Discord Bot
     - Send Messages Permission
+    - Send Embed Permission
 - Webhook
 - Python (https://www.python.org/downloads/)
     - Discord Module (https://pypi.org/project/discord.py/)
@@ -13,23 +14,63 @@ Grabs emotes from all servers its in and sends a webhook to the URL specified
 
 
 ## How to Use
-- Create a webhook in the channel you would like the messages sent to
 - In the folder you will be running the bot from, create a file named .env
     - Format it the folowing way, replacing the things in <> with what it should be
     ```
     TOKEN=<BOT TOKEN>
+    ```
+### <u>Custom Channel Method</u>
+- Once you have added the token to the `.env` file, run the `main.py` file
+
+<u>Command Usage</u>
+
+Arguments:
+- Position 1
+    ```
+    #channel (This will be a channel mention where you would like it to send)
+    ```
+- Position 2
+    ```
+    -g (global will be every emote the bot has access to)
+    -l (local will be every emote in the server you run the command)
+    ```
+Full Command usage
+
+- Example 1: `$grab-emotes #channel -g`
+- Example 2: `$grab-emotes #channel -l`
+
+### <u>Predefined Webhook Method</u>
+- Create a webhook in the channel you would like the messages sent to
+- Add the folowing line to the `.env` file previusly made
+    - Format it the folowing way, replacing the things in <> with what it should be
+    ```
     WEBHOOK_URL=<WEBHOOK URL>
     ```
 
--  In the file called main.py you can edit the **optional** variables
+-  In the file called `main.py` you can edit the **optional** variables
     ```
-    WEBHOOK_NAME = "The name you want the webhook to have"
-    WEBHOOK_AVATAR = "A link to the file which should be the bots avatar"
+    WEBHOOK_NAME (The name you want the webhook to have)
+    WEBHOOK_AVATAR (A link to the file which should be the bots avatar)
     ```
 
-- Once you have done all of that, run the main.py file
-- Run the command `$grab-emotes` in any channel the bot can see
-    - Do note that the bot will take all emotes from all servers it is in, not just the server you run the command in
+- Once you have aded the webhook link, you can run the `main.py` file
+
+<u>Command Usage</u>
+
+Arguments:
+- Position 1
+    ```
+    -w (This indicates you would like to send through the webhook you made)
+    ```
+- Position 2
+    ```
+    -g (global will be every emote the bot has access to)
+    -l (local will be every emote in the server you run the command)
+    ```
+Full Command usage
+
+- Example 1: `$grab-emotes -w -g`
+- Example 2: `$grab-emotes -w -l`
 
 Thats all!
 
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from pydoc import describe
+from turtle import color
 import discord
-import discord.ext.commands
+from discord.ext import commands
 from dotenv import load_dotenv
 import os
 import requests
@@ -8,6 +10,7 @@ import time
 load_dotenv()
 
 client = discord.Client()
+client = commands.Bot(command_prefix="$")
 TOKEN = os.getenv("TOKEN")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 WEBHOOK_NAME = ""
@@ -17,46 +20,71 @@ WEBHOOK_AVATAR = ""
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
+
+@client.command(name="grab-emotes")
+async def grabEmote(ctx, channel, *args):
+    channel = channel
+
+    try:
+        if channel == "-w":
+            channel = "webhook"
+        else:
+            channel = channel.replace("<#", "")
+            channel = channel.replace(">", "")
+            a = ctx.guild.get_channel(int(channel))
+            channel = a
+    except ValueError as err:
+        em = discord.Embed(title="Invalid Argument", color=discord.Colour.from_rgb(241, 79, 64), description="Invalid argument in position 1. Please provide a valid argument.\n```\n\"-w\" uses configured webook\n\"channel mention\" uses mentioned channel\n\nEX: $grab-emotes #emotes -g\nEX2: $grab-emotes -w -l```")
+        await ctx.channel.send(embed=em)
+        print(err)
         return
 
-    if message.content.startswith('$grab-emotes'):
-        m = await message.channel.send('grabing. . .')
-        
-        print(client.emojis)
-        print("-------------")
+    try:
+        if args[0] == "-g":    
+            await get_all(ctx, channel)
+        elif args[0] == "-l":
+            await get_guild(ctx, channel)
+    except IndexError as err:
+        em = discord.Embed(title="Invalid Argument", color=discord.Colour.from_rgb(241, 79, 64), description="Invalid argument in position 2. Please provide a valid argument.\n```\n\"-g\" searches global\n\"-l\" searches only this server\n\nEX: $grab-emotes #emotes -g\nEX2: $grab-emotes -w -l```")
+        await ctx.channel.send(embed=em)
+        print(err)
 
-        string = [""]
-        num = 0
 
-        for a in client.emojis:
-            if a.animated:
-                if checks(num, string, f"<a:{a.name}:{a.id}> -> `<a:{a.name}:{a.id}>`\n"):
-                    string[num]+=f"<a:{a.name}:{a.id}> -> `<a:{a.name}:{a.id}>`\n"
-                    print(f"<a:{a.name}:{a.id}> -> `<a:{a.name}:{a.id}>`\n")
-                else:
-                    num+=1
-                    string.append("")
-                    string[num]+=f"<a:{a.name}:{a.id}> -> `<a:{a.name}:{a.id}>`\n"
-                    print(f"<a:{a.name}:{a.id}> -> `<a:{a.name}:{a.id}>`\n") 
+async def get_all(message, channel):
+    m = await message.channel.send('grabing. . .')
+
+    print(client.emojis)
+    print("-------------")
+
+    string = [""]
+    num = 0
+
+    for a in client.emojis:
+        if a.animated:
+            if checks(num, string, f"<a:{a.name}:{a.id}> -> `<a:{a.name}:{a.id}>`\n"):
+                string[num]+=f"<a:{a.name}:{a.id}> -> `<a:{a.name}:{a.id}>`\n"
+                print(f"<a:{a.name}:{a.id}> -> `<a:{a.name}:{a.id}>`\n")
             else:
-                if checks(num, string, f"<:{a.name}:{a.id}> -> `<:{a.name}:{a.id}>`\n"):
-                    string[num]+=f"<:{a.name}:{a.id}> -> `<:{a.name}:{a.id}>`\n"
-                    print(f"<:{a.name}:{a.id}> -> `<:{a.name}:{a.id}>`\n")
-                else:
-                    num+=1
-                    string.append("")
-                    string[num]+=f"<:{a.name}:{a.id}> -> `<:{a.name}:{a.id}>`\n"
-                    print(f"<:{a.name}:{a.id}> -> `<:{a.name}:{a.id}>`\n")
+                num+=1
+                string.append("")
+                string[num]+=f"<a:{a.name}:{a.id}> -> `<a:{a.name}:{a.id}>`\n"
+                print(f"<a:{a.name}:{a.id}> -> `<a:{a.name}:{a.id}>`\n") 
+        else:
+            if checks(num, string, f"<:{a.name}:{a.id}> -> `<:{a.name}:{a.id}>`\n"):
+                string[num]+=f"<:{a.name}:{a.id}> -> `<:{a.name}:{a.id}>`\n"
+                print(f"<:{a.name}:{a.id}> -> `<:{a.name}:{a.id}>`\n")
+            else:
+                num+=1
+                string.append("")
+                string[num]+=f"<:{a.name}:{a.id}> -> `<:{a.name}:{a.id}>`\n"
+                print(f"<:{a.name}:{a.id}> -> `<:{a.name}:{a.id}>`\n")
 
 
-        print("-------------")
-        print(string)
-        print("-------------")
+    print("-------------")
+    print(string)
+    print("-------------")
 
-
+    if channel == "webhook":
         for indx, b in enumerate(string):
             print(b)
             data = {
@@ -67,16 +95,81 @@ async def on_message(message):
 
 
             res = requests.post(WEBHOOK_URL, json=data)
-            time.sleep(5)
+            time.sleep(1)
 
-        try:
-            res.raise_for_status()
-        except requests.exceptions.HTTPError as err:
-            print(err)
+            try:
+                res.raise_for_status()
+            except requests.exceptions.HTTPError as err:
+                print(err)
+            else:
+                print(f"[WEB HOOK] Successfully sent, Code {res.status_code}")
+    else:
+        for indx, b in enumerate(string):
+            await channel.send(string[indx])
+            time.sleep(1)
+
+    await m.edit(content="Done!")
+
+async def get_guild(message, channel):
+    m = await message.channel.send('grabing. . .')
+
+    print(message.guild.emojis)
+    print("-------------")
+
+    string = [""]
+    num = 0
+
+    for a in message.guild.emojis:
+        if a.animated:
+            if checks(num, string, f"<a:{a.name}:{a.id}> -> `<a:{a.name}:{a.id}>`\n"):
+                string[num]+=f"<a:{a.name}:{a.id}> -> `<a:{a.name}:{a.id}>`\n"
+                print(f"<a:{a.name}:{a.id}> -> `<a:{a.name}:{a.id}>`\n")
+            else:
+                num+=1
+                string.append("")
+                string[num]+=f"<a:{a.name}:{a.id}> -> `<a:{a.name}:{a.id}>`\n"
+                print(f"<a:{a.name}:{a.id}> -> `<a:{a.name}:{a.id}>`\n") 
         else:
-            print(f"[WEB HOOK] Successfully sent, Code {res.status_code}")
+            if checks(num, string, f"<:{a.name}:{a.id}> -> `<:{a.name}:{a.id}>`\n"):
+                string[num]+=f"<:{a.name}:{a.id}> -> `<:{a.name}:{a.id}>`\n"
+                print(f"<:{a.name}:{a.id}> -> `<:{a.name}:{a.id}>`\n")
+            else:
+                num+=1
+                string.append("")
+                string[num]+=f"<:{a.name}:{a.id}> -> `<:{a.name}:{a.id}>`\n"
+                print(f"<:{a.name}:{a.id}> -> `<:{a.name}:{a.id}>`\n")
 
-        await m.edit(content="Done!")
+
+    print("-------------")
+    print(string)
+    print("-------------")
+
+    if channel == "webhook":
+        for indx, b in enumerate(string):
+            print(b)
+            data = {
+                "username": WEBHOOK_NAME,
+                "avatar_url": WEBHOOK_AVATAR,
+                "content": string[indx]
+            }
+
+
+            res = requests.post(WEBHOOK_URL, json=data)
+            time.sleep(1)
+
+            try:
+                res.raise_for_status()
+            except requests.exceptions.HTTPError as err:
+                print(err)
+            else:
+                print(f"[WEB HOOK] Successfully sent, Code {res.status_code}")
+    else:
+        for indx, b in enumerate(string):
+            await channel.send(string[indx])
+            time.sleep(1)
+
+
+    await m.edit(content="Done!")
 
         
 def checks(num, arr, new_string):
